a22769bb1df6be242c977c0dd990080f88374bd1
Diego Alfonso Oderigo <diegoalfonso.oderigo@bbva.com>
2018-06-21 19:23:44 -0300

    Agrego actualizacion de Productos Vendidos

--- compile.sh
+++ compile.sh
@@ -2,7 +2,9 @@
 cobc -x tp.clp -o target/tp.exe
 cobc -c updprodven.clp
 cobc -c -x testupdprodven.clp
-cobc -x tp2.clp -o target/tp2.exe
+cobc -c -x tp2.clp
 cobc -x -o target/test.exe testupdprodven.o updprodven.o
+cobc -x -o target/tp2.exe tp2.o updprodven.o
 rm testupdprodven.o
 rm updprodven.o
+rm tp2.o
